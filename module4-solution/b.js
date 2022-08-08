6769aa5643a7841334e15fb86fbfc88605927cc6
Selammoll <106996680+Selammoll@users.noreply.github.com>
2022-08-08 13:36:06 +0300

    Add files via upload

---
+++ module4-solution/b.js
@@ -0,0 +1,13 @@
+var arry=["Jason","kaylin","ben","suzi","johnson","talyor","kan","braod","john","yakoov"];
+
+for(var i=0; i<arry.length; i++   ){
+
+
+	if(arry[i].charAt(0)==='J' || arry[i].charAt(0)==='j' ){
+		console.log("goodbye "  +  arry[i])
+	}
+	else{
+		console.log("hello "   + arry[i])
+	}
+
+}
